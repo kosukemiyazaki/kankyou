7a0e1e41ca972b3739ed8ecbfb82421fe26e82e4
kosukemiyazaki <73573588+kosukemiyazaki@users.noreply.github.com>
2023-12-25 18:34:11 +0900

    Add files via upload

--- dcdc_sim_contisim.m
+++ dcdc_sim_contisim.m
@@ -20,16 +20,16 @@ err_cnt = 0;
 for cnt = 2:ptn
  cnt_str = num2str(cnt-1);
  ptn_str = num2str(ptn-1);
- mag = waitbar((cnt-2)/(ptn-1)),['連続シミュレーション実行中...(',cnt_str,'/',ptn_str,')']; %waitbarの表示
+ msg = waitbar((cnt-2)/(ptn-1)),['連続シミュレーション実行中...(',cnt_str,'/',ptn_str,')']); %waitbarの表示
  trycnt = 0;
  senarioName = cell2mat(raw(cnt,1)); %テストパターン名定義
  
  for listchk = 2:2 %testdata_listの設定ミスをチェック
   %テストパターン名未記入、設定値未記入、設定値に数字以外を記入した場合エラー
-  if isnan(cell2mat(raw,(cnt,1)))
+  if isnan(cell2mat(raw(cnt,1)))
   err_cnt = err_cnt + 1;
   trycnt = 1;
-  errorcomment{end+1,1} = [num2str(error_cnt),'.testdata_listの'num2str(cnt),'列目のパターン名に誤りがあります。未記入になっていないかなどを確認してください。'];
+  errorcomment{end+1,1} = [num2str(error_cnt),'.testdata_listの',num2str(cnt),'列目のパターン名に誤りがあります。未記入になっていないかなどを確認してください。'];
   break;
   end
  end
@@ -37,7 +37,7 @@ for cnt = 2:ptn
  if trycnt == 0
   try 
    dcdc_sim_demo_forloop; %sim実行
-  ctach
+  catch
   end
  end
  if trycnt == 0
@@ -55,11 +55,11 @@ for cnt = 2:ptn
   if trycnt == 1
    %データ保存用フォルダの作成
    folder_name = [scenarioName,'_エラー'];
-    if exist(['.\sim_result\'.folder_name],'dir') == 0
-     mkdir(['.\sim_result',folder_name]);
+    if exist(['.\sim_result\',folder_name],'dir') == 0
+     mkdir(['.\sim_result\',folder_name]);
      end
      %matファイルを保存
-     matFileName = ['.\sim_result',folder_name,'\',scenarioName,'_err.mat'];
+     matFileName = ['.\sim_result\',folder_name,'\',scenarioName,'_error.mat'];
      save(matFileName);
     end
  catch
@@ -67,7 +67,7 @@ for cnt = 2:ptn
  
  end
  
- error_log_for_loop;%エラーログの作成
+ %error_log_for_loop;%エラーログの作成
  
  try
   close all hidden
